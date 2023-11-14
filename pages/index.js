72a7eac1e10f02e3c3989599bdab1c08ec0572c4
somin <smhs12@naver.com>
2023-11-14 14:57:01 +0900

    feat: index 페이지 메타데이터 작성

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import { fetchCountries } from "@/api";
 import CountryList from "@/components/CountryList";
 import Searchbar from "@/components/SearchBar";
+import Head from "next/head";
 
 export default function Home({ countries }) {
   return (
     <>
+      <Head>
+        <title>NARAS</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="NARAS" />
+        <meta property="og:description" content="전 세계 국가들의 정보를 확인해보세요" />
+      </Head>
       <Searchbar />
       <CountryList countries={countries} />
     </>
