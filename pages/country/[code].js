07bac0a10dd91d29613c0efefb1b4a4750736a3d
somin <smhs12@naver.com>
2023-11-14 14:48:21 +0900

    style: Country 페이지 완성

--- pages/country/[code].js
+++ pages/country/[code].js
@@ -1,6 +1,8 @@
 import { fetchCountry } from "@/api";
 import SubLayout from "@/components/SubLayout";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import style from "./[code].module.css";
 
 export default function Country({ country }) {
   const router = useRouter();
@@ -14,8 +16,35 @@ export default function Country({ country }) {
   }
 
   return (
-    <div>
-      {country.commonName} {country.officialName}
+    <div className={style.container}>
+      <div className={style.header}>
+        <div className={style.commonName}>
+          {country.flagEmoji}&nbsp;{country.commonName}
+        </div>
+        <div className={style.officialName}>{country.officialName}</div>
+      </div>
+
+      <div className={style.flag_img}>
+        <Image src={country.flagImg} alt={country.commonName} fill />
+      </div>
+
+      <div className={style.body}>
+        <div>
+          <b>코드 :</b>&nbsp;{country.code}
+        </div>
+        <div>
+          <b>수도 :</b>&nbsp;{country.capital.join(", ")}
+        </div>
+        <div>
+          <b>지역 :</b>&nbsp;{country.region}
+        </div>
+        <div>
+          <b>지도 :</b>&nbsp;
+          <a target="_blank" href={country.googleMapURL}>
+            {country.googleMapURL}
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
@@ -24,7 +53,7 @@ Country.Layout = SubLayout;
 
 export const getStaticPaths = async () => {
   return {
-    paths: [{ params: { code: "ABW" } }],
+    paths: [{ params: { code: "ABW" } }, { params: { code: "KOR" } }, { params: { code: "CAN" } }],
     fallback: "blocking",
     //paths에 지정하지 않은 경우의 요청이 들어왔을 때 처리하는 옵션
     //blocking: paths에 존재하지 않으면 실시간으로 생성하여 정적 페이지 생성
