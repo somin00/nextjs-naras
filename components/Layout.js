a59c31aa62c8473dbdef4e6a85d62eb0ff20926c
somin <smhs12@naver.com>
2023-11-14 14:19:03 +0900

    feat: 로고 클릭시 홈페이지로 이동하는 라우터 추가

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,15 @@
+import { useRouter } from "next/router";
 import styles from "./Layout.module.css";
 export default function Layout({ children }) {
+  const router = useRouter();
+  const clickHeader = () => {
+    router.push("/");
+  };
   return (
     <div>
-      <header className={styles.header}>NARAS 🌏</header>
+      <header className={styles.header} onClick={clickHeader}>
+        NARAS 🌏
+      </header>
       <main className={styles.main}>{children}</main>
     </div>
   );
